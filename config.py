7dafe89245921aa41305bb4f20e9bbdf2ec653b9
Hollings <hollingsfsdf@gmail.com>
2017-09-04 17:51:12 -0700

    Fixed app is not defined error

--- config.py
+++ config.py
@@ -7,6 +7,11 @@ if os.environ.get('DATABASE_URL') is None:
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
+    
+from flask import Flask, render_template
+import sys
 import logging
+app = Flask(__name__)
+
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
