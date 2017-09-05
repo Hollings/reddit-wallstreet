789fdad62adcd7eaf15c5a14f51dce22fa6c2721
Hollings <hollingsfsdf@gmail.com>
2017-09-04 18:04:07 -0700

    more gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
+if __name__ == '__main__':
+    app.run()
 from app import views, models
