71458cecd899e2b98f1bf4e394a674666f2d02b8
Hollings <hollingsfsdf@gmail.com>
2017-09-04 17:57:32 -0700

    Trying to get debug working

--- config.py
+++ config.py
@@ -7,11 +7,10 @@ if os.environ.get('DATABASE_URL') is None:
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-    
 from flask import Flask, render_template
 import sys
 import logging
 app = Flask(__name__)
-
+app.debug = True
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
