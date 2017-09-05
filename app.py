23a08b0c56d3e871cd8c13d5557e633aa63ce1bd
Hollings <hollingsfsdf@gmail.com>
2017-09-04 18:08:10 -0700

    asdf

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import logging
+import sys
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,5 +12,5 @@ app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
     app.run()
-    
+
 from app import views, models
