0e89316bfd4359eb8857c55eaead5c81a62856b0
Hollings <hollingsfsdf@gmail.com>
2017-09-04 17:44:26 -0700

    Send logs to Heroku

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
